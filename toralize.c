83ad6121e463d45ac87621f3b4e7f27d2fadacee
ssebide <sebiddejohn136@gmail.com>
2025-07-17 12:20:51 +0300

    Turn client to llbrary, replace regular connect

--- toralize.c
+++ toralize.c
@@ -1,8 +1,15 @@
 /* toralize.c*/
 
+/*
+1. Turn the client into a library (shared library) .so
+2. Turn main() into our own connect()
+3. replace regular connect()
+4. Grab the IP and port from ORIG connect()
+*/
+
 #include "toralize.h"
 
-Req *request(const char *dstip, const int dstport)
+Req *request(struct sockaddr_in *sock2)
 {
     Req *req;
 
@@ -10,34 +17,25 @@ Req *request(const char *dstip, const int dstport)
 
     req->vn = 4;
     req->cd = 1;
-    req->dstport = htons(dstport);
-    req->dstip = inet_addr(dstip);
+    req->dstport = sock2->sin_port;
+    req->dstip = sock2->sin_addr.s_addr;
     strncpy(req->userid, USERNAME, 8);
 
     return req;
 }
 
-int main(int argc, char *argv[])
+int connect(int s2, const struct sockaddr *sock2, socklen_t addrlen)
 {
-    char *host;
-    int port, s;
+    int s;
     struct sockaddr_in sock;
     Req *req;
     Res *res;
     char buf[ressize];
     int succeess;
     char tmp[512];
+    int (*p)(int, const struct sockaddr *, socklen_t);
 
-    if (argc < 3)
-    {
-        fprintf(stderr, "Usage: %s <host> <port>\n",
-                argv[0]);
-
-        return -1;
-    }
-
-    host = argv[1];
-    port = atoi(argv[2]);
+    p = dlsym(RTLD_NEXT, "connect");
 
     s = socket(AF_INET, SOCK_STREAM, 0);
     if (s < 0)
@@ -51,7 +49,7 @@ int main(int argc, char *argv[])
     sock.sin_port = htons(PROXYPORT);
     sock.sin_addr.s_addr = inet_addr(PROXY);
 
-    if (connect(s, (struct sockaddr *)&sock, sizeof(sock)))
+    if (p(s, (struct sockaddr *)&sock, sizeof(sock)))
     {
         perror("connect");
 
@@ -60,7 +58,7 @@ int main(int argc, char *argv[])
 
     printf("Connected to proxy\n");
 
-    req = request(host, port);
+    req = request((struct sockaddr_in *)&sock2);
     write(s, req, reqsize);
 
     memset(buf, 0, ressize);
@@ -85,20 +83,10 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    printf("Successfully connected through the proxy to %s:%d\n", host, port);
+    printf("Successfully connected through the proxy.\n");
 
-    memset(tmp, 0, 512);
-    snprintf(tmp, 511,
-             "HEAD / HTTP/1.0\r\n"
-             "Host: www.networktechnology.org\r\n"
-             "\r\n");
-    write(s, tmp, strlen(tmp));
+    dup2(s, s2);
 
-    memset(tmp, 0, 512);
-    read(s, tmp, 511);
-    printf("'%s'\n", tmp);
-
-    close(s);
     free(req);
 
     return 0;
--- toralize.h
+++ toralize.h
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <dlfcn.h>
 
 #define PROXY "127.0.0.1"
 #define PROXYPORT 9050
@@ -52,5 +53,5 @@ struct proxy_response
 
 typedef struct proxy_response Res;
 
-Req *request(const char *, const int);
-int main(int, char **);
+Req *request(struct sockaddr_in *);
+int connect(int, const struct sockaddr *, socklen_t);
